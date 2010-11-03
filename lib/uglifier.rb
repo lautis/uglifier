a3a6593acbcb1281102983220169c2067be7b0d7
Ville Lautanala <lautis@gmail.com>
2010-11-03 22:54:50 +0200

    Write something to readme about usage

--- lib/uglifier.rb
+++ lib/uglifier.rb
@@ -5,12 +5,12 @@ class Uglifier
   class Error < StandardError; end
 
   DEFAULTS = {
-    :mangle => true,
-    :toplevel => false,
-    :squeeze => true,
-    :seqs => true,
-    :dead_code => true,
-    :beautify => false,
+    :mangle => true, # Mangle variables names
+    :toplevel => false, # Mangle top-level variable names
+    :squeeze => true, # Squeeze code resulting in smaller, but less-readable code
+    :seqs => true, # Reduce consecutive statements in blocks into single statement
+    :dead_code => true, # Remove dead code (e.g. after return)
+    :beautify => false, # Ouput indented code
     :beautify_options => {
       :indent_level => 4,
       :indent_start => 0,
