6c725f8348e27fea9616524c08b3619e5e0b9b70
Ville Lautanala <lautis@gmail.com>
2013-10-19 19:27:40 +0300

    Fix specs on Ruby 1.8.7

--- spec/uglifier_spec.rb
+++ spec/uglifier_spec.rb
@@ -166,7 +166,7 @@ describe "Uglifier" do
   it "quotes unsafe keys by default" do
     code = 'var code = {"class": "", "\u200c":"A"}'
     Uglifier.compile(code).should include('"class"')
-    Uglifier.compile(code, output: {quote_keys: false}).should include("\"\u200c\"")
+    Uglifier.compile(code, :output => {:quote_keys => false}).should include("\"\u200c\"")
   end
 
   it "handles constant definitions" do
