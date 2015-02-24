7ff1a3398ae1e5f41067e21d806a5ef2e8d76af7
Jon Bardin <diclophis@gmail.com>
2015-02-23 16:44:08 -0800

    wrap uglifier js context in anon self-executing function for compatibility with alaska execjs runtime

--- lib/uglifier.rb
+++ lib/uglifier.rb
@@ -10,67 +10,69 @@ class Uglifier
   Error = ExecJS::Error
   # JavaScript code to call UglifyJS
   JS = <<-JS
-    function comments(option) {
-      if (Object.prototype.toString.call(option) === '[object Array]') {
-        return new RegExp(option[0], option[1]);
-      } else if (option == "jsdoc") {
-        return function(node, comment) {
-          if (comment.type == "comment2") {
-            return /@preserve|@license|@cc_on/i.test(comment.value);
-          } else {
-            return false;
+    (function() {
+      function comments(option) {
+        if (Object.prototype.toString.call(option) === '[object Array]') {
+          return new RegExp(option[0], option[1]);
+        } else if (option == "jsdoc") {
+          return function(node, comment) {
+            if (comment.type == "comment2") {
+              return /@preserve|@license|@cc_on/i.test(comment.value);
+            } else {
+              return false;
+            }
           }
+        } else {
+          return option;
         }
-      } else {
-        return option;
       }
-    }
 
-    var options = %s;
-    var source = options.source;
-    var ast = UglifyJS.parse(source, options.parse_options);
-    ast.figure_out_scope();
-
-    if (options.compress) {
-      var compressor = UglifyJS.Compressor(options.compress);
-      ast = ast.transform(compressor);
+      var options = %s;
+      var source = options.source;
+      var ast = UglifyJS.parse(source, options.parse_options);
       ast.figure_out_scope();
-    }
 
-    if (options.mangle) {
-      ast.compute_char_frequency();
-      ast.mangle_names(options.mangle);
-    }
+      if (options.compress) {
+        var compressor = UglifyJS.Compressor(options.compress);
+        ast = ast.transform(compressor);
+        ast.figure_out_scope();
+      }
 
-    if (options.enclose) {
-      ast = ast.wrap_enclose(options.enclose);
-    }
+      if (options.mangle) {
+        ast.compute_char_frequency();
+        ast.mangle_names(options.mangle);
+      }
 
-    var gen_code_options = options.output;
-    gen_code_options.comments = comments(options.output.comments);
+      if (options.enclose) {
+        ast = ast.wrap_enclose(options.enclose);
+      }
 
-    if (options.generate_map) {
-        var source_map = UglifyJS.SourceMap(options.source_map_options);
-        gen_code_options.source_map = source_map;
-    }
+      var gen_code_options = options.output;
+      gen_code_options.comments = comments(options.output.comments);
 
-    var stream = UglifyJS.OutputStream(gen_code_options);
+      if (options.generate_map) {
+          var source_map = UglifyJS.SourceMap(options.source_map_options);
+          gen_code_options.source_map = source_map;
+      }
 
-    ast.print(stream);
+      var stream = UglifyJS.OutputStream(gen_code_options);
 
-    if (options.source_map_options.map_url) {
-      stream += "\\n//# sourceMappingURL=" + options.source_map_options.map_url;
-    }
+      ast.print(stream);
 
-    if (options.source_map_options.url) {
-      stream += "\\n//# sourceURL=" + options.source_map_options.url;
-    }
+      if (options.source_map_options.map_url) {
+        stream += "\\n//# sourceMappingURL=" + options.source_map_options.map_url;
+      }
 
-    if (options.generate_map) {
-        return [stream.toString(), source_map.toString()];
-    } else {
-        return stream.toString();
-    }
+      if (options.source_map_options.url) {
+        stream += "\\n//# sourceURL=" + options.source_map_options.url;
+      }
+
+      if (options.generate_map) {
+          return [stream.toString(), source_map.toString()];
+      } else {
+          return stream.toString();
+      }
+    })()
   JS
 
   # UglifyJS source path
